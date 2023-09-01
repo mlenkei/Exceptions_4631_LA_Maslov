626304a804b15eb84e3ac0924c6bd79bccfef4cc
Leonid Maslov <mlenkei@mail.ru>
2023-09-01 13:31:28 +0500

    DZ_2

---
+++ dz_2.java
@@ -0,0 +1,36 @@
+// Если необходимо, исправьте данный код
+// Задание 2
+// try {
+//    int d = 0;
+//    double catchedRes1 = intArray[8] / d;
+//    System.out.println("catchedRes1 = " + catchedRes1);
+// } catch (ArithmeticException e) {
+//    System.out.println("Catching exception: " + e);
+// }
+
+import java.io.FileNotFoundException;
+
+public class dz_2 {
+        public static void main(String[] args) throws Exception {
+        try {
+            int a = 90;
+            int b = 3;
+            System.out.println(a / b);
+            printSum(23, 234);
+            int[] abc = {1, 2};
+            abc[3] = 9;
+        } catch (NullPointerException ex) {
+            System.out.println("Указатель не может указывать на null!");
+        } catch (IndexOutOfBoundsException ex) {
+            System.out.println("Массив выходит за пределы своего размера!");
+        } catch (Throwable ex) {
+            System.out.println("Что-то пошло не так...");
+        }
+    }
+
+    public static void printSum(Integer a, Integer b) throws FileNotFoundException {
+        System.out.println(a + b);
+    }
+
+}
+
